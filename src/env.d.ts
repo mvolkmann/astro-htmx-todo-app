3d4d7cff596a3a31d1d1958fb6395985e98fbc27
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-12-23 10:43:59 -0600

    cleanup

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,8 @@
 /// <reference types="astro/client" />
 
+// Astro.locals is an object that enables setting and sharing
+// request-specific information across endpoints and pages.
+// This is available in all Astro components and API endpoints.
 declare namespace App {
   interface Locals {
     filter: import('@data/todos').TodoFilter;
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,22 @@
 import {defineMiddleware} from 'astro:middleware';
 import {z} from 'astro/zod';
 
-// probably a better way to share this around! This list needs to match the enum in data/todos.ts
+// There is probably a better way to share this!
+// This list needs to match the enum in data/todos.ts.
 const filterSchema = z.enum(['all', 'active', 'completed']).default('all');
 
-// `context` and `next` are automatically typed
+// See https://docs.astro.build/en/guides/middleware/.
+// `context` and `next` are automatically typed.
 export const onRequest = defineMiddleware(async (context, next) => {
   if (context.request.method === 'GET') {
     context.locals.filter = filterSchema.parse(
       context.url.searchParams.get('filter')?.toString()
     );
   } else {
-    // the current filter needs to be included for when partials are requested
-    // pages/index.astro uses `hx-vals` to make sure the filter is included with
-    // every HTMX request. Here we strip that back out and share it in the request's context
+    // The current filter needs to be included for when partials are requested.
+    // pages/index.astro uses `hx-vals` to make sure the filter
+    // is included with every HTMX request.
+    // Here we strip that back out and share it in the request's context.
     const formData = await context.request.clone().formData();
     context.locals.filter = filterSchema.parse(formData.get('filter'));
   }
