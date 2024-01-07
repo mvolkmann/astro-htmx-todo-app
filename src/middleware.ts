8c36e6ac9f3ebf6bb71b75465bf9a1b1f040a123
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-01-07 12:17:10 -0600

    progress

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,22 @@
 import {defineMiddleware} from 'astro:middleware';
 import {z} from 'astro/zod';
 
-// There is probably a better way to share this!
 // This list needs to match the enum in data/todos.ts.
 const filterSchema = z.enum(['all', 'active', 'completed']).default('all');
 
-// See https://docs.astro.build/en/guides/middleware/.
-// `context` and `next` are automatically typed.
+// Every request passes through this middleware.
+// Its purpose is to update the context with the current filter value.
+// It would be easier to just use sessionStorage!
 export const onRequest = defineMiddleware(async (context, next) => {
+  // For all GET requests, copy the "filter" query parameter into the context.
   if (context.request.method === 'GET') {
     context.locals.filter = filterSchema.parse(
       context.url.searchParams.get('filter')?.toString()
     );
   } else {
+    // For all non-GET requests, copy the "filter" value
+    // from the form data into the context.
+
     // The current filter needs to be included for when partials are requested.
     // pages/index.astro uses `hx-vals` to make sure the filter
     // is included with every HTMX request.
@@ -21,5 +25,7 @@ export const onRequest = defineMiddleware(async (context, next) => {
     context.locals.filter = filterSchema.parse(formData.get('filter'));
   }
 
+  // src/pages/index.astro includes the filter value in the context
+  // when it sends a POST to /partials/todos for adding a new todo.
   return next();
 });
